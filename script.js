067d47f7bbc3c4f743401cf629229e54fbffa251
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2022-01-17 19:02:30 +0300

    Add seat number array

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ let numberOfSeats = document.querySelectorAll(".seat").length;
 let ticketsCount = document.getElementById("count");
 let totalSum = document.getElementById("total");
 let movieSelect = document.getElementById("movie");
+let movieSeatsNumbers = [];
 //console.log(seats);
 let selectedSeatCounter = 0;
 
 for (let i = 0; i < numberOfSeats; i++) {
     document.querySelectorAll(".seat")[i].addEventListener("click", function() {
-        if (this.classList.contains("occupied") === false) {
+        if (this.classList.contains("occupied") === false && this.nodeName === "DIV") {
             if (this.classList.contains("selected") === false) {
                 this.classList.add("selected");
                 selectedSeatCounter = selectedSeatCounter + 1;
+                movieSeatsNumbers.push(this.id);
             } else {
                 this.classList.remove("selected");
                 selectedSeatCounter = selectedSeatCounter - 1;
+                let movieSeatIndex =  movieSeatsNumbers.indexOf(this.id);
+                movieSeatsNumbers.splice(movieSeatIndex, 1);
             }
         }
         ticketsCount.innerHTML = selectedSeatCounter;
         totalSum.innerHTML = parseInt(movieSelect.value) * selectedSeatCounter;
-        console.log(selectedSeatCounter);
+        console.log(movieSeatsNumbers);
     });
 }
 
