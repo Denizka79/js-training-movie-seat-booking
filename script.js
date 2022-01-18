af54022eef51e83e8811e7b4b8b6876510976561
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2022-01-18 19:17:19 +0300

    Finish project

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
+let allSeats = document.querySelectorAll(".seat");
 let numberOfSeats = document.querySelectorAll(".seat").length;
 let ticketsCount = document.getElementById("count");
 let totalSum = document.getElementById("total");
 let movieSelect = document.getElementById("movie");
 let movieSeatsNumbers = [];
-//console.log(seats);
 let selectedSeatCounter = 0;
 
+function populateUI() {
+    let selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+    let updatedMovieSeatNumbers = [];
+    if (selectedSeats != null && selectedSeats.length > 0) {
+        for (let i = 0; i < numberOfSeats; i++) {
+            for (let j = 0; j < selectedSeats.length; j++) {
+                if (allSeats[i].id === selectedSeats[j]) {
+                    allSeats[i].classList.add("selected");
+                    updatedMovieSeatNumbers.push(allSeats[i].id);
+                }
+            }
+        }
+        movieSeatsNumbers = updatedMovieSeatNumbers;
+        selectedSeatCounter = selectedSeats.length;
+        ticketsCount.innerHTML = selectedSeatCounter;
+    }
+
+    let selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+        totalSum.innerHTML = parseInt(movieSelect.value) * selectedSeatCounter;
+    } else {
+        totalSum.innerHTML = parseInt(movieSelect.value) * selectedSeatCounter;
+    }
+}
+
+populateUI();
+
 for (let i = 0; i < numberOfSeats; i++) {
-    document.querySelectorAll(".seat")[i].addEventListener("click", function() {
-        if (this.classList.contains("occupied") === false && this.nodeName === "DIV") {
-            if (this.classList.contains("selected") === false) {
-                this.classList.add("selected");
+    allSeats[i].addEventListener("click", function() {
+        if (allSeats[i].classList.contains("occupied") === false && allSeats[i].nodeName === "DIV") {
+            if (allSeats[i].classList.contains("selected") === false) {
+                allSeats[i].classList.add("selected");
                 selectedSeatCounter = selectedSeatCounter + 1;
-                movieSeatsNumbers.push(this.id);
+                movieSeatsNumbers.push(allSeats[i].id);
             } else {
-                this.classList.remove("selected");
+                allSeats[i].classList.remove("selected");
                 selectedSeatCounter = selectedSeatCounter - 1;
-                let movieSeatIndex =  movieSeatsNumbers.indexOf(this.id);
+                let movieSeatIndex =  movieSeatsNumbers.indexOf(allSeats[i].id);
                 movieSeatsNumbers.splice(movieSeatIndex, 1);
             }
         }
         ticketsCount.innerHTML = selectedSeatCounter;
         totalSum.innerHTML = parseInt(movieSelect.value) * selectedSeatCounter;
         localStorage.setItem("selectedSeats", JSON.stringify(movieSeatsNumbers));
-        console.log(movieSeatsNumbers);
     });
 }
 
